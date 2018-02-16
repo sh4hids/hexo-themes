83894d7b14247dd4c2cadd382193fe3efc988c7e
5h4hid5 <shahid.bracu@gmail.com>
2018-02-16 09:14:34 +0600

    Added pageview

--- themes/programmers/source/js/script.js
+++ themes/programmers/source/js/script.js
@@ -1,137 +1,56 @@
-(function($){
-  // Search
-  var $searchWrap = $('#search-form-wrap'),
-    isSearchAnim = false,
-    searchAnimDuration = 200;
+(function($) {
 
-  var startSearchAnim = function(){
-    isSearchAnim = true;
+  function toBengaliNum(t) {
+    var r = {
+      0: "০",
+      1: "১",
+      2: "২",
+      3: "৩",
+      4: "৪",
+      5: "৫",
+      6: "৬",
+      7: "৭",
+      8: "৮",
+      9: "৯",
+      ".": ".",
+      "-": "-"
+    };
+    if (isNaN(parseFloat(t)) || isNaN(t - 0))
+      return "Invalid input type";
+    var i = "";
+    return (t.toString().split("").forEach(function(t) {
+      return (i += r[t]);
+    }), i);
+  }
+
+  var config = {
+    apiKey: "AIzaSyDxjeY8j3iuVtNnxZInXq5--NrduBbw6pA",
+    authDomain: "prozuktischool-analytics.firebaseapp.com",
+    databaseURL: "https://prozuktischool-analytics.firebaseio.com",
+    projectId: "prozuktischool-analytics",
+    storageBucket: "prozuktischool-analytics.appspot.com",
+    messagingSenderId: "834432059721"
   };
+  firebase.initializeApp(config);
 
-  var stopSearchAnim = function(callback){
-    setTimeout(function(){
-      isSearchAnim = false;
-      callback && callback();
-    }, searchAnimDuration);
-  };
-
-  $('#nav-search-btn').on('click', function(){
-    if (isSearchAnim) return;
-
-    startSearchAnim();
-    $searchWrap.addClass('on');
-    stopSearchAnim(function(){
-      $('.search-form-input').focus();
-    });
-  });
-
-  $('.search-form-input').on('blur', function(){
-    startSearchAnim();
-    $searchWrap.removeClass('on');
-    stopSearchAnim();
-  });
-
-  // Share
-  $('body').on('click', function(){
-    $('.article-share-box.on').removeClass('on');
-  }).on('click', '.article-share-link', function(e){
-    e.stopPropagation();
-
-    var $this = $(this),
-      url = $this.attr('data-url'),
-      encodedUrl = encodeURIComponent(url),
-      id = 'article-share-box-' + $this.attr('data-id'),
-      offset = $this.offset();
-
-    if ($('#' + id).length){
-      var box = $('#' + id);
-
-      if (box.hasClass('on')){
-        box.removeClass('on');
-        return;
+  var database = firebase.database();
+  var oriUrl = window.location.host;
+  var curUrl = oriUrl + window.location.pathname;
+  function readVisits(url, selector) {
+    var db_key = decodeURI(url.replace(new RegExp('\\/|\\.', 'g'), "_"));
+    database.ref(db_key).once("value").then(function(result) {
+      var count = parseInt(result.val() || 0) + 1;
+      var countBn = toBengaliNum(count);
+      if (url.indexOf("localhost:4000") === -1) {
+        database.ref(db_key).set(count);
       }
-    } else {
-      var html = [
-        '<div id="' + id + '" class="article-share-box">',
-          '<input class="article-share-input" value="' + url + '">',
-          '<div class="article-share-links">',
-            '<a href="https://twitter.com/intent/tweet?url=' + encodedUrl + '" class="article-share-twitter" target="_blank" title="Twitter"></a>',
-            '<a href="https://www.facebook.com/sharer.php?u=' + encodedUrl + '" class="article-share-facebook" target="_blank" title="Facebook"></a>',
-            '<a href="http://pinterest.com/pin/create/button/?url=' + encodedUrl + '" class="article-share-pinterest" target="_blank" title="Pinterest"></a>',
-            '<a href="https://plus.google.com/share?url=' + encodedUrl + '" class="article-share-google" target="_blank" title="Google+"></a>',
-          '</div>',
-        '</div>'
-      ].join('');
-
-      var box = $(html);
-
-      $('body').append(box);
-    }
-
-    $('.article-share-box.on').hide();
-
-    box.css({
-      top: offset.top + 25,
-      left: offset.left
-    }).addClass('on');
-  }).on('click', '.article-share-box', function(e){
-    e.stopPropagation();
-  }).on('click', '.article-share-box-input', function(){
-    $(this).select();
-  }).on('click', '.article-share-box-link', function(e){
-    e.preventDefault();
-    e.stopPropagation();
-
-    window.open(this.href, 'article-share-box-window-' + Date.now(), 'width=500,height=450');
-  });
-
-  // Caption
-  $('.article-entry').each(function(i){
-    $(this).find('img').each(function(){
-      if ($(this).parent().hasClass('fancybox')) return;
-
-      var alt = this.alt;
-
-      if (alt) $(this).after('<span class="caption">' + alt + '</span>');
-
-      $(this).wrap('<a href="' + this.src + '" title="' + alt + '" class="fancybox"></a>');
+      if (selector.length > 0) {
+        selector.html(countBn);
+      };
     });
-
-    $(this).find('.fancybox').each(function(){
-      $(this).attr('rel', 'article' + i);
-    });
-  });
-
-  if ($.fancybox){
-    $('.fancybox').fancybox();
   }
-
-  // Mobile nav
-  var $container = $('#container'),
-    isMobileNavAnim = false,
-    mobileNavAnimDuration = 200;
-
-  var startMobileNavAnim = function(){
-    isMobileNavAnim = true;
-  };
-
-  var stopMobileNavAnim = function(){
-    setTimeout(function(){
-      isMobileNavAnim = false;
-    }, mobileNavAnimDuration);
+  readVisits(oriUrl, $("#visits .count"));
+  if (curUrl && curUrl != "_") {
+    readVisits("page/" + curUrl, $("#pageviews .count"));
   }
-
-  $('#main-nav-toggle').on('click', function(){
-    if (isMobileNavAnim) return;
-
-    startMobileNavAnim();
-    $container.toggleClass('mobile-nav-on');
-    stopMobileNavAnim();
-  });
-
-  $('#wrap').on('click', function(){
-    if (isMobileNavAnim || !$container.hasClass('mobile-nav-on')) return;
-
-    $container.removeClass('mobile-nav-on');
-  });
 })(jQuery);
